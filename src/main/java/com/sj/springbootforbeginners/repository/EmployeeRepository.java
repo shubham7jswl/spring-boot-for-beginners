a038ff45bde56338d2eb1c1dda19a4b9f1ab56ec
shubhan7jswl <shubham7jaiswal@gmail.com>
2023-02-15 23:47:04 -0600

    changes for springBoot for beginners with Spring jpa module

--- src/main/java/com/sj/springbootforbeginners/repository/EmployeeRepository.java
+++ src/main/java/com/sj/springbootforbeginners/repository/EmployeeRepository.java
@@ -4,9 +4,6 @@ import com.sj.springbootforbeginners.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-
-
 // @Repository is a Spring annotation that
 // indicates that the decorated class is a repository.
 @Repository
