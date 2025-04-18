92e5693c229ad7411c2d2ead904b54e8ae460ec4
shubhamjaiswal <shubham7jaiswal@gmail.com>
2025-04-18 17:45:38 -0500

    google sheet parsing changes changes

--- mysqlSetup.sh
+++ mysqlSetup.sh
@@ -1,4 +1,6 @@
+#commands to run if you want a msql terminal based client
 docker run --name=mysql-container -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:latest
 docker exec -it mysql-container mysql -u root -p
-docker run --name=mysql-container -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 -d mysql:latest
 
+#commands to run if you want a msql desktop based client like dbeaver etc.
+docker run --name=mysql-container -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 -d mysql:latest
