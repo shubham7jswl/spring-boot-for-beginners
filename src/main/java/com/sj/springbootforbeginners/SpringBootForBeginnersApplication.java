146907393b63a9f773c8673b506d5671608ba896
shubhamjaiswal <shubham7jaiswal@gmail.com>
2025-04-18 17:33:11 -0500

    google sheet parsing changes changes

--- src/main/java/com/sj/springbootforbeginners/SpringBootForBeginnersApplication.java
+++ src/main/java/com/sj/springbootforbeginners/SpringBootForBeginnersApplication.java
@@ -1,14 +1,10 @@
 package com.sj.springbootforbeginners;
 
-//import com.sj.springbootforbeginners.service.GoogleSheetsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+
 
 @EnableScheduling
 @SpringBootApplication
